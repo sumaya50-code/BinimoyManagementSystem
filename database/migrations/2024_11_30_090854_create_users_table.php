7f2530ef8fedf1356f9b5048729909c09a9c1b7d
sumaya50-code <sumayaakter50tri@gmail.com>
2025-11-30 15:32:18 +0600

    changes table

--- database/migrations/2024_11_30_090854_create_users_table.php
+++ database/migrations/2024_11_30_090854_create_users_table.php
@@ -6,17 +6,18 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('role_id')->constrained('roles')->cascadeOnDelete(); // Role relationship
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('phone')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('status', ['active', 'inactive'])->default('active'); // Active / Inactive
+            $table->timestamp('last_login_at')->nullable(); // Track last login
             $table->rememberToken();
             $table->timestamps();
         });
@@ -37,13 +38,10 @@ return new class extends Migration
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
-        Schema::dropIfExists('users');
-        Schema::dropIfExists('password_reset_tokens');
         Schema::dropIfExists('sessions');
+        Schema::dropIfExists('password_reset_tokens');
+        Schema::dropIfExists('users');
     }
 };
